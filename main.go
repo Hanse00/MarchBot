373b970abd77e1ecd1a029817679154d7f6d49a9
Philip Mallegol-Hansen <philip.mh@slalom.com>
2024-01-03 11:08:02 -0800

    Basic posting
    We implement basic posting to Mastodon.
    Currently the message is hardcoded and cannot be changed.
    No real error handling exists either.

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	key := os.Getenv("MASTODON_KEY")
+
+	form := url.Values{}
+	form.Add("status", "Hello World!")
+	form.Add("visibility", "public")
+	form.Add("language", "eng")
+
+	request, err := http.NewRequest("POST", "https://mastodon.mallegolhansen.com/api/v1/statuses", strings.NewReader(form.Encode()))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", key))
+
+	response, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
 }
